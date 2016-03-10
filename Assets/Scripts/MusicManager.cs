83cf2379ec7369142f5a15742fd4f6b1f57963fe
uichunter <pankettingpanda@gmail.com>
2016-03-10 00:38:16 -0600

    Change the OnlevelWasLoaded method.

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,16 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour {	
 	public AudioClip[] levelMusicChangeArray;
+	private AudioSource music;
 
+	void Awake ()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+			music = GetComponent<AudioSource> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnLevelWasLoaded (int level)
+	{
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		if (thisLevelMusic) {
+			music.clip = thisLevelMusic;
+			if (level > 0) {
+				music.loop = true;
+			}else{
+				music.loop = false;
+			}
+		Debug.Log(level+" music is  "+music.clip);
+		music.Play();
+		}
 	}
+
+
 }
