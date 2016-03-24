0b6ead742a68f5a4048e8e23d83a7a6f28adbf9f
uichunter <pankettingpanda@gmail.com>
2016-03-23 20:54:51 -0500

    Edited Attacker.cs.   -When target destroyed change the isAttacking to false.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,15 +5,20 @@ public class Attacker : MonoBehaviour {
 	//[Range(-1f,2f)] we dont need this anymore;
 	private float currentSpeed;
 	private GameObject currentTarget;
+	private Animator atkAnimator;
 
 	// Use this for initialization
 	void Start () {
-	
+		atkAnimator = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Translate(Vector3.left*currentSpeed*Time.deltaTime);
+	void Update ()
+	{
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+		if (!currentTarget) {
+			atkAnimator.SetBool("isAttacking",false);
+		}
 	}
 
 	void OnTriggerEnter2D ()
