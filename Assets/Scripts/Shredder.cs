5343e92c0675c13098485d24022a2edf784e9bb3
uichunter <pankettingpanda@gmail.com>
2016-04-11 21:55:06 -0500

    I add a palyer coin function to shredder. But world value controller may be better.

--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 
 public class Shredder : MonoBehaviour {
+	public int playerCoins =1;
+	LvManager lvManager;
 
+	// I think I should make a WorldValue controller;
 	void OnTriggerEnter2D (Collider2D collider)
 	{
 		Destroy (collider.gameObject);
+		if (collider.gameObject.GetComponent<Attacker> ()) {
+			playerCoins -= 1;
+			if (playerCoins <= 0) {
+				lvManager = GetComponent<LvManager>();
+				lvManager.LvLoader("03b Lose");
+			}
+		}
 	}
 }
