d4bfa7ec6fe294cc709cc368f0a68575e1368977
uichunter <pankettingpanda@gmail.com>
2016-04-12 21:51:22 -0500

    Add loose collider. Remember instantiate lv mananger prefab. In OnTriggerEnter2D method Destroy collider dosen`t work. Remember only can destroy collider.gameobject.

---
+++ Assets/LooseColider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseColider : MonoBehaviour {
+
+	public int playerCoins;
+	LvManager lvManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerCoins = 3;
+
+		lvManager = GameObject.FindObjectOfType<LvManager> ();
+		if (lvManager == null) {
+			Debug.LogError(name+" can`t find any Lv manager.");
+		}
+	}
+
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Destroy (collider.gameObject);
+		if (collider.gameObject.GetComponent<Attacker> ()) {
+			playerCoins -= 1;
+			if (playerCoins <= 0) {
+				lvManager.LvLoader("03b Lose");
+			}
+		}
+
+	}
+}
